7da0ac048d56c784683b02ea233f28b24cfb256e
rishi5-isyntech <rishikesh.waghunde@isynergytech.com>
2021-11-08 11:38:43 +0530

    env commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-    apiUrl: 'http://localhost:8080'
-    //apiUrl: 'http://49.248.214.214:8080'
+    //apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://49.248.214.214:8080'
 };
 
 /*
